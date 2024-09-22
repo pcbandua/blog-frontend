01dee65bf3558ee250632c6350733f370785e3c3
Paulo Bandua <pcbandua@gmail.com>
2024-09-22 07:22:35 -0700

    addded RESTful buttons to frontend

--- src/ArtPage.jsx
+++ src/ArtPage.jsx
@@ -14,21 +14,46 @@ export function ArtPage(){
   
 // INDEX 
   const getArtPostData = () => {
-    console.log('hello');
+    console.log('get data');
     axios.get("http://localhost:3000/posts.json").then(response => {
       console.log(response.data);
       setPosts(response.data)
     })
   }
 
-  // // CREATE
-  // const createArtPostData = () => {
-  //   console.log('hello');
-  //   axios.post("http://localhost:3000/posts.json").then(response =>{console.log(response)
-  //   })
-  // }
+// CREATE
+  const createArtPostData = () => {
+    console.log('create data');
+    axios.post("http://localhost:3000/posts.json").then(response =>{console.log(response.data)
+      setPosts(response.data)
+    })
+  }
 
+// READ
+  const readArtPostData =  () => {
+    console.log("Read Data");
+    axios.get("http://localhost:3000/posts.json").then(response =>{console.log(response.data)
+      setPosts(response.data)
+    })
+  }
 
+// UPDATE
+const updateArtPostData =  () => {
+  console.log("Update Data");
+  axios.patch("http://localhost:3000/posts/1.json").then(response =>{console.log(response.data)
+    setPosts(response.data)
+  })
+}
+
+// DELETE
+const deleteArtPostData =  () => {
+  console.log("Delete Data");
+  axios.delete("http://localhost:3000/posts/1.json").then(response =>{console.log(response.data)
+    setPosts(response.data)
+  })
+}
+
+  // MODAL
   const showModal = () => {
     setIsPostsShowVisible(true);
   };
@@ -51,6 +76,10 @@ export function ArtPage(){
   <PostsNew/>
   <PostsIndex myFirstName={firstName} artPosts={posts} onShow={showModal}/>
   <button onClick={getArtPostData}>get Art Posts</button>
+  <button onClick={createArtPostData}>create Art Posts</button>
+  <button onClick={readArtPostData}>read Art Posts</button>
+  <button onClick={updateArtPostData}>update Art Posts</button>
+  <button onClick={deleteArtPostData}>delete Art Posts</button>
   <Modal show={isPostsShowVisible} onClose={closeModal}>
         <p>TEST Content</p>
   </Modal>
